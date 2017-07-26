f5b5cce977102244d331b9a07dc03bfc885a1b0b
Maple <angelmeng86@163.com>
2017-07-26 12:30:02 +0800

    1、登录验证信息显示错误

--- cloud.js
+++ cloud.js
@@ -245,8 +245,10 @@ AV.Cloud.afterUpdate('BusinessApply', function(request) {
 });
 
 AV.Cloud.onLogin(function(request) {
-  if (request.object.get('active') == false) {
-    // 如果是 error 回调，则用户无法登录（收到 401 响应）
-    throw new AV.Cloud.Error('该用户已被禁用');
-  }
+	if(request.object.has('active')) {
+		if (request.object.get('active') == false) {
+	    // 如果是 error 回调，则用户无法登录（收到 401 响应）
+	    throw new AV.Cloud.Error('该用户已被禁用');
+	  }
+	}
 });
